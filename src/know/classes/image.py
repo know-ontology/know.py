94a10788e11fa7b7dc479e0b53949cd4837c4c76
Arto Bendiken <arto@asimov.systems>
2025-10-16 20:11:36 +0400

    Enable updating image data.

--- src/know/classes/image.py
+++ src/know/classes/image.py
@@ -1,7 +1,7 @@
 # This is free and unencumbered software released into the public domain.
 
-from base64 import b64decode
-from pydantic import BaseModel, Field
+from base64 import b64encode, b64decode
+from pydantic import BaseModel, Field, computed_field
 import PIL.Image
 
 
@@ -12,9 +12,14 @@ class Image(BaseModel):
     height: int
     data_url: str = Field(..., alias="data")
 
+    @computed_field
     @property
     def data(self) -> bytes:
         return b64decode(self.data_url.split(",")[1])
+
+    @data.setter
+    def data(self, new_data: bytes):
+        self.data_url = f"data:image/rgb;base64,{b64encode(new_data).decode()}"
 
     def decode(self) -> PIL.Image:
         return PIL.Image.frombytes("RGB", (self.width, self.height), self.data)
